54b32f254da8c4f0e70fece730c52ffe4b6d8b4a
Kooville <ville.kuhlberg@helsinki.fi>
2026-09-15 13:03:50 +0300

    Refactored unit test code to remove repetition

--- test/test_camera_movement.gd
+++ test/test_camera_movement.gd
@@ -1,47 +1,29 @@
 extends GdUnitTestSuite
 
-var runner:GdUnitSceneRunner
-var camera:Camera3D
-var start_dir:Vector3
-
-func before_test():
+func test_mouse_movement(direction:Vector2, axis:String, increase:bool, _test_parameters := [
+	[Vector2(0, -100), "x", true],
+	[Vector2(0, 100), "x", false],
+	[Vector2(-100, 0), "y", true],
+	[Vector2(100, 0), "y", false]
+]):
 	# Create a scene
-	runner = scene_runner("res://Scenes/World.tscn")
-	camera = runner.find_child("Camera3D")
-	start_dir = camera.global_rotation_degrees
-
-func test_mouse_up():
-	# Simulate mouse moving up
-	runner.simulate_mouse_move_relative(Vector2(0, -100), 2.0)
+	var	runner := scene_runner("res://Scenes/World.tscn")
+	var camera:Camera3D = runner.find_child("Camera3D")
+	var start_dir:Vector3 = camera.global_rotation_degrees
+	
+	# Simulate mouse movement
+	runner.simulate_mouse_move_relative(direction, 2.0)
 	await await_millis(2000)
 
-	# Camera rotates upwards around x axis
+	# Camera rotates around chosen axis
 	var end_dir:Vector3 = camera.global_rotation_degrees
-	assert_float(end_dir.x).is_greater(start_dir.x)
-
-func test_mouse_down():
-	# Simulate mouse moving down
-	runner.simulate_mouse_move_relative(Vector2(0, 100), 2.0)
-	await await_millis(2000)
-
-	# Camera rotates down around x axis
-	var end_dir:Vector3 = camera.global_rotation_degrees
-	assert_float(end_dir.x).is_less(start_dir.x)
-
-func test_mouse_left():
-	# Simulate mouse moving left
-	runner.simulate_mouse_move_relative(Vector2(-100, 0), 2.0)
-	await await_millis(2000)
-
-	# Camera rotates left around y axis
-	var end_dir:Vector3 = camera.global_rotation_degrees
-	assert_float(end_dir.y).is_greater(start_dir.y)
-
-func test_mouse_right():
-	# Simulate mouse moving right
-	runner.simulate_mouse_move_relative(Vector2(100, 0), 2.0)
-	await await_millis(2000)
-
-	# Camera rotates right around y axis
-	var end_dir:Vector3 = camera.global_rotation_degrees
-	assert_float(end_dir.y).is_less(start_dir.y)
+	if axis == "x":
+		if increase == true:
+			assert_float(end_dir.x).is_greater(start_dir.x)
+		else:
+			assert_float(end_dir.x).is_less(start_dir.x)
+	else:
+		if increase == true:
+			assert_float(end_dir.y).is_greater(start_dir.y)
+		else:
+			assert_float(end_dir.y).is_less(start_dir.y)
--- test/test_movement.gd
+++ test/test_movement.gd
@@ -2,61 +2,31 @@ extends GdUnitTestSuite
 
 var runner:GdUnitSceneRunner
 var player:CharacterBody3D
-var start_position:Vector3
 
 func before_test():
-	# Create a scene
+	# Setup the scene
 	runner = scene_runner("res://Scenes/World.tscn")
 	player = runner.find_child("Player")
-	start_position = player.global_position
 
-func test_move_right():
-	# Simulate input for key D
-	runner.simulate_key_press(KEY_D)
+func test_player_movement(key:Key, direction:Vector3, _test_parameters := [
+	[KEY_A, Vector3.LEFT],
+	[KEY_D, Vector3.RIGHT],
+	[KEY_W, Vector3.FORWARD],
+	[KEY_S, Vector3.BACK]
+	]):
+
+	# Get start position
+	var start_position:Vector3 = player.global_position
+
+	# Simulate input for movement
+	runner.simulate_key_press(key)
 	await await_millis(1500)
-	runner.simulate_key_release(KEY_D)
+	runner.simulate_key_release(key)
 
-	# Player moves to the right
+	# Player moves to the correct direction
 	var end_position:Vector3 = player.global_position
-	
 	var movement_direction:Vector3 = start_position.direction_to(end_position)
-	assert_vector(movement_direction).is_equal_approx(Vector3.RIGHT, Vector3(0.01, 0.1, 0.01))
-
-func test_move_left():
-	# Simulate input for key A
-	runner.simulate_key_press(KEY_A)
-	await await_millis(1500)
-	runner.simulate_key_release(KEY_A)
-
-	# Player moves to the left
-	var end_position:Vector3 = player.global_position
-	
-	var movement_direction:Vector3 = start_position.direction_to(end_position)
-	assert_vector(movement_direction).is_equal_approx(Vector3.LEFT, Vector3(0.01, 0.1, 0.01))
-
-func test_move_fwd():
-	# Simulate input for key W
-	runner.simulate_key_press(KEY_W)
-	await await_millis(1500)
-	runner.simulate_key_release(KEY_W)
-
-	# Player moves forward
-	var end_position:Vector3 = player.global_position
-	
-	var movement_direction:Vector3 = start_position.direction_to(end_position)
-	assert_vector(movement_direction).is_equal_approx(Vector3.FORWARD, Vector3(0.01, 0.1, 0.01))
-
-func test_move_back():
-	# Simulate input for key D
-	runner.simulate_key_press(KEY_S)
-	await await_millis(1500)
-	runner.simulate_key_release(KEY_S)
-
-	# Player moves backwards
-	var end_position:Vector3 = player.global_position
-	
-	var movement_direction:Vector3 = start_position.direction_to(end_position)
-	assert_vector(movement_direction).is_equal_approx(Vector3.BACK, Vector3(0.01, 0.1, 0.01))
+	assert_vector(movement_direction).is_equal_approx(direction, Vector3(0.01, 0.1, 0.01))
 
 func test_jump():
 	# Simulate input for key SPACE
